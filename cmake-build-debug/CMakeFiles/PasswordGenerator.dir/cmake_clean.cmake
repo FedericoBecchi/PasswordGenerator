19e7a202a3e7d11cdefa636b05e203cffcca4360
FedericoBecchi <84383138+FedericoBecchi@users.noreply.github.com>
2024-02-15 22:11:58 +0100

    01

---
+++ cmake-build-debug/CMakeFiles/PasswordGenerator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PasswordGenerator.dir/main.cpp.obj"
+  "CMakeFiles/PasswordGenerator.dir/main.cpp.obj.d"
+  "PasswordGenerator.exe"
+  "PasswordGenerator.exe.manifest"
+  "PasswordGenerator.pdb"
+  "libPasswordGenerator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PasswordGenerator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -3,40 +3,57 @@
 #include <random>
 using namespace std;
 
-#define LUNGHEZZA_MAX 20 // Indica il numero massimo di caratteri della password
-#define LUNGHEZZA_MIN 10 // Indica il numero minimo di caratteri della password
+#define LUNGHEZZA_MAX 40 // Indica il numero massimo di caratteri della password
+#define LUNGHEZZA_MIN 15 // Indica il numero minimo di caratteri della password
 
 #define NUMERO_MINIMO_MAIUSCOLE   1   // Indica il numero minimo di caratteri maiuscoli della password
-#define NUMERO_MINIMO_MINUSCOLE   2   // Indica il numero minimo di caratteri minuscoli della password
-#define NUMERO_MINIMO_NUMERI      1   // Indica il numero minimo di numeri che deve essere presente
+#define NUMERO_MINIMO_MINUSCOLE   1   // Indica il numero minimo di caratteri minuscoli della password
+#define NUMERO_MINIMO_NUMERI      18  // Indica il numero minimo di numeri che deve essere presente
 #define NUMERO_CARATTERI_SPECIALI 1   // Indica il numero minimo di caratteri della password
 
 #define PRIMO_CARATTERE_STAMPABILE  33
 #define ULTIMO_CARATTERE_STAMPABILE 126
 
+int generate_number_in_range(const int start,const int end){
+    random_device rd1; // obtain a random number from hardware
+    mt19937 gen1(rd1()); // seed the generator
+    uniform_int_distribution<> distr1(start, end); // define the range
+
+    return distr1(gen1);
+}
+
 string genera_password(){
     string password;
     int lunghezza_password;
+    vector<int> posizioni;
 
-    random_device rd1; // obtain a random number from hardware
-    mt19937 gen1(rd1()); // seed the generator
-    uniform_int_distribution<> distr1(LUNGHEZZA_MIN, LUNGHEZZA_MAX); // define the range
 
-    lunghezza_password = distr1(gen1);
+    // GENERAZIONE DELLA LUNGHEZZA DELLA STRINGA
+    lunghezza_password = generate_number_in_range(LUNGHEZZA_MIN,LUNGHEZZA_MAX);
 
-    random_device rd2; // obtain a random number from hardware
-    mt19937 gen2(rd2()); // seed the generator
-    uniform_int_distribution<> distr2(PRIMO_CARATTERE_STAMPABILE, ULTIMO_CARATTERE_STAMPABILE); // define the range
+    //POPOLAZIONE STRINGA CON SPAZI POPOLAZIONE VECTOR CON POSIZIONI;
 
     for(int i = 0 ; i < lunghezza_password ; i++) {
-        char ch = distr2(gen2);
-        password+=ch;
-    }
+        password += ' ';
+        posizioni.push_back(i);
+    };
+
+
+    // GENERAZIONE ELEMENTI COME CARATTERI SPECIALI
+
+
+    // GENERAZIONE ELEMENTI COME NUMERI
+
+    // GENERAZIONE ELEMENTI COME MAIUSCOLE
+
+    // GENERAZIONE ELEMENTI COME MINUSCOLE
+
 
     return password;
 }
 
-bool isSpecial(char c){
+
+bool isspecial(char c){
     bool isspecial;
     (c >= '!' and c <= '/') or (c >= ':' and  c<= '@') or (c >= '[' and  c <= 96) or (c >= '{' and  c <= 126) ? isspecial = true : isspecial = false;
     return isspecial;
@@ -48,26 +65,31 @@ bool check_password(string password){
     int count_numeri = 0;
     int count_speciali = 0;
 
-
     for(int i = 0 ; i < password.length(); i++){
+        char examined_char = password[i];
         isupper(password[i])    ? count_maiuscole++ : count_maiuscole;
         islower(password[i])    ? count_minuscole++ : count_minuscole;
-        isnumber(password[i])   ? count_numeri++    : count_numeri;
-        isSpecial(password[i])  ? count_speciali++  : count_speciali;
+        isdigit(password[i])    ? count_numeri++    : count_numeri;
+        isspecial(password[i])  ? count_speciali++  : count_speciali;
     }
 
-    return count_maiuscole >= NUMERO_MINIMO_MAIUSCOLE and count_minuscole >= NUMERO_MINIMO_MINUSCOLE and count_numeri >= NUMERO_MINIMO_NUMERI and count_speciali >= NUMERO_CARATTERI_SPECIALI;
+    bool ret = (count_maiuscole >= NUMERO_MINIMO_MAIUSCOLE) and (count_minuscole >= NUMERO_MINIMO_MINUSCOLE) and (count_numeri >= NUMERO_MINIMO_NUMERI) and (count_speciali >= NUMERO_CARATTERI_SPECIALI);
+
+    return ret;
 }
+
 
 int main() {
 
-    string password;
+/*    string password;
 
     do {
         password = genera_password();
-    } while (check_password(password));
+    } while (!check_password(password));
 
-    cout << password;
+    cout << password;*/
+
+    vector<int> el;
 
     return 0;
 }
